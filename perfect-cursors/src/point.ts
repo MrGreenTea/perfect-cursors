ce36f68e0a0e1b68b4faf7a56d13a4c6aee8c785
Jonas Bulik <jonas@bulik.dev>
2025-04-13 18:38:12 +0200

    chore: format with prettier

--- perfect-cursors/src/point.ts
+++ perfect-cursors/src/point.ts
@@ -1,5 +1,5 @@
-export type Point2D = [number, number];
+export type Point2D = [number, number]
 
 export function dist(a: Point2D, b: Point2D): number {
-    return Math.hypot(a[1] - b[1], a[0] - b[0]);
+  return Math.hypot(a[1] - b[1], a[0] - b[0])
 }
